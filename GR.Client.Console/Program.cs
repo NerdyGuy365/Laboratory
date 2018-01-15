de036672e69417ebac0fece1496a8bbf8fb7aae9
Mark Richardson <MarkRichardson@curo.com>
2018-01-15 17:08:12 -0600

    Updated code into the code a more singular code base.
    Still need to test a few things.

---
+++ GR.Client.Console/Program.cs
@@ -0,0 +1,88 @@
+﻿using GR.Business;
+using GR.Client.Console.SelfHostingREST;
+using GR.Contracts.DataContracts;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GR.Client.Console
+{
+    public class Program
+    {
+        //Form scoper variables section.
+        private static List<Person> _people = new List<Person>();
+        private static PersonHttpClient _serviceClient = new PersonHttpClient();
+
+        private static string _serviceEndPoint = ConfigurationManager.AppSettings["RESTURI"];
+
+        private static Thread _serviceThread;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //See Actions Folder in project.
+
+                //Combine the 3 files into a single set of records.
+                foreach(var file in args)
+                    _people.AddRange(PersonHelpers.ParsePersonDataFromFile(file));
+
+                //Start REST service.
+                _serviceThread = new Thread(StartRESTService);
+                _serviceThread.Start();
+                Thread.Sleep(3000); 
+                
+                //Let's post at least 5 different lines to the service.
+                var result = PostLine(1);
+                result = PostLine(2);
+                result = PostLine(3);
+                result = PostLine(4);
+                result = PostLine(5);
+
+                System.Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                //An exception can occur when dealing with files. 
+                //Since this is a demo. Just throw a dummy message.
+                System.Console.WriteLine("It looks like you might have have a file in the correct place. Please double check the LocalData folder.");
+            }
+        }
+
+        /// <summary>
+        /// This is a helper method used to post all data.
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<string> PostLine(int lineNumber)
+        {
+            try
+            {
+                return await _serviceClient.PostRecord(_people[lineNumber - 1]).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                //Since the service will display a friendly error.
+                //Let the service message inform the user.
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// This method will start the REST service.
+        /// </summary>
+        private static void StartRESTService()
+        {
+            using (WebApp.Start<Startup>(_serviceEndPoint))
+            {
+                System.Console.WriteLine("Web Server is running.");
+                System.Console.WriteLine("Press any key to quit.");
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ GR.Client.Console/SelfHostingREST/PersonREST.cs
@@ -0,0 +1,80 @@
+﻿using GR.Contracts.DataContracts;
+using GR.Data;
+using GR.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GR.Client.Console.SelfHostingREST
+{
+    /// <summary>
+    /// The purpose of this class is to handle incoming request from a client.
+    /// </summary>
+    public class PersonREST : ApiController
+    {
+        //Form scope variables
+        private List<Person> _people = new List<Person>();
+
+        /// <summary>
+        /// Used to get a listing of people sorted by birth dates.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Records/Birthdate")]
+        public List<Person> GetBirthDates()
+        {
+            //Make sure we have something to return.
+            //Let the client know if we don't.
+            if (_people == null || _people.Count == 0) throw new Exception("No records found");
+
+            //Get results and sort correctly. 
+            return _people.OrderBy(p => p.DateOfBirth).ToList();
+        }
+
+        /// <summary>
+        /// Used to get a listing of people sorted by genders.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Records/Gender")]
+        public List<Person> GetGenders()
+        {
+            //Make sure we have something to return.
+            //Let the client know if we don't.l
+            if (_people == null || _people.Count == 0) throw new Exception("No records found");
+
+            //Get results and sort correctly.
+            return _people.OrderBy(p => p.Gender).ThenBy(l => l.LastName).ToList();
+        }
+
+        /// <summary>
+        /// Used to get a listing of people sorted by names.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Records/Name")]
+        public List<Person> GetNames()
+        {
+            //Make sure we have something to return.
+            //Let the client know if we don't.
+            if (_people == null || _people.Count == 0) throw new Exception("No records found");
+
+            //Get results and sort correctly.
+            return _people.OrderByDescending(p => p.LastName).ToList();
+        }
+
+        /// <summary>
+        /// Used to save a person.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Records")]
+        public void SavePerson([FromBody]Person person)
+        {
+            _people.Add(person);
+        }
+    }
+}
